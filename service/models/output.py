1cfd9255a4819a8686f2c52a3c4f551e130d74cd
volbil <volbilnexus@gmail.com>
2022-08-18 13:07:05 +0300

    Models cleanup

--- service/models/output.py
+++ service/models/output.py
@@ -4,10 +4,10 @@ from typing import Optional
 from .base import BaseTable
 
 class Output(BaseTable, table=True):
-    txid: str
     amount: Optional[condecimal(max_digits=20, decimal_places=8)] = Field(default=0)
-    currency: str
     spent: Optional[bool] = Field(default=False)
+    currency: str
+    txid: str
 
     transaction_id: Optional[int] = Field(default=None, foreign_key="transaction.id")
     transaction: Optional["Transaction"] = Relationship(back_populates="outputs")
--- service/models/transaction.py
+++ service/models/transaction.py
@@ -4,11 +4,17 @@ from pydantic import condecimal
 from .base import BaseTable
 
 class Transaction(BaseTable, table=True):
-    raw_tx: str
-    txid: Optional[str] = Field(default=None)
-    send_token: Optional[str] = Field(default=None)
-    send_amount: Optional[condecimal(max_digits=20, decimal_places=8)] = Field(default=0)
-    receive_token: Optional[str] = Field(default=None)
-    receive_amount: Optional[condecimal(max_digits=20, decimal_places=8)] = Field(default=0)
-    closed: bool = Field(default=True)
+    receive_amount: Optional[
+        condecimal(max_digits=20, decimal_places=8)
+    ] = Field(default=0)
+
+    send_amount: Optional[
+        condecimal(max_digits=20, decimal_places=8)
+    ] = Field(default=0)
+
     outputs: List["Output"] = Relationship(back_populates="transaction")
+    receive_token: Optional[str] = Field(default=None)
+    send_token: Optional[str] = Field(default=None)
+    txid: Optional[str] = Field(default=None)
+    closed: bool = Field(default=True)
+    raw_tx: str
